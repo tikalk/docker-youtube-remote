e96a05dc3e66cf1d4c04106f92087ad76345afca
liya <liya@tikalk.com>
2015-12-14 10:34:36 +0200

    update repos names

--- build.sh
+++ build.sh
@@ -18,17 +18,17 @@ echo "WORKSPACE=$WORKSPACE"
 
 echo "Build ynginx image for version ${YR_VERSION}"
 cd ${WORKSPACE}/ynginx
-docker build -t ${DOCKER_REGISTRY}/ynginx:${YR_VERSION} .
-docker push ${DOCKER_REGISTRY}/ynginx:${YR_VERSION}
-docker tag -f ${DOCKER_REGISTRY}/ynginx:${YR_VERSION} ${DOCKER_REGISTRY}/ynginx:latest-not-tested
-docker push ${DOCKER_REGISTRY}/ynginx:latest-not-tested
+docker build -t ${DOCKER_REGISTRY}/yremote/ynginx:${YR_VERSION} .
+docker push ${DOCKER_REGISTRY}/yremote//ynginx:${YR_VERSION}
+docker tag -f ${DOCKER_REGISTRY}/yremote/ynginx:${YR_VERSION} ${DOCKER_REGISTRY}/yremote/ynginx:latest-not-tested
+docker push ${DOCKER_REGISTRY}/yremote/ynginx:latest-not-tested
 echo
 echo "Build yremote image for version ${YR_VERSION}"
 cd ${WORKSPACE}/yremote
-docker build -t ${DOCKER_REGISTRY}/yremote:${YR_VERSION} .
-docker push ${DOCKER_REGISTRY}/yremote:${YR_VERSION}
-docker tag -f ${DOCKER_REGISTRY}/yremote:${YR_VERSION} ${DOCKER_REGISTRY}/yremote:latest-not-tested
-docker push ${DOCKER_REGISTRY}/yremote:latest-not-tested
+docker build -t ${DOCKER_REGISTRY}/yremote/yremote:${YR_VERSION} .
+docker push ${DOCKER_REGISTRY}/yremote/yremote:${YR_VERSION}
+docker tag -f ${DOCKER_REGISTRY}/yremote/yremote:${YR_VERSION} ${DOCKER_REGISTRY}/yremote/yremote:latest-not-tested
+docker push ${DOCKER_REGISTRY}/yremote/yremote:latest-not-tested
 
 
 
