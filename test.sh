e903320396ce2526b2d6c2adb05c5e5623aa507a
Miron Gofer <miron@tikalk.com>
2015-12-19 01:48:51 +0200

    Adding a small test

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+echo "Downloading the app front page"
+wget -o /tmp/index.html http://localhost:81 &1> /dev/null
+
+echo "Verifying that it is \'youtubeRemoteApp\' page"
+cat /tmp/index.html |grep "<body ng-app=\"youtubeRemoteApp\">" > /dev/null 
+EXIT=$?
+
+echo "cleaning up"
+rm /tmp/index.html
+if [[ $EXIT != 0 ]]; then
+	OUT='Success';
+else
+	OUT='Failure';
+fi
+echo "Test status is $OUT"
+exit $EXIT
