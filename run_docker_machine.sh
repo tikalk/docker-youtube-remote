b0a8cd38ba71a768624f9c1fdeed5878e6b44f98
liya <liya@tikalk.com>
2015-12-23 19:02:56 +0200

    add workaround to replace version in docker-compose

--- run_docker_machine.sh
+++ run_docker_machine.sh
@@ -2,20 +2,21 @@
 
 DOCKER_REGISTRY="dreg.tikalknowledge.com:5000"
 
-export VERSION=$1
-if [[ -z $VERSION ]]
+export YR_VERSION=$1
+if [[ -z $YR_VERSION ]]
 then
 	 echo "Version is not defined!"
      exit 1
 fi
-MACHINE_NAME=yremote-$VERSION
+echo "YR_VERSION=$YR_VERSION"
+MACHINE_NAME=yremote-$YR_VERSION
 
 export DOCKER_MACHINE_DRIVER=virtualbox
 if (( $# > 1 )); then
     export DOCKER_MACHINE_DRIVER=$2
 fi
 
-echo "DOCKER_MACHINE_DRIVER si $DOCKER_MACHINE_DRIVER"
+echo "DOCKER_MACHINE_DRIVER is $DOCKER_MACHINE_DRIVER"
 if [ "$DOCKER_MACHINE_DRIVER" == "aws" ];then
 		
 	echo "Set AWS keys.."
@@ -37,12 +38,12 @@ if [ "$DOCKER_MACHINE_DRIVER" == "aws" ];then
 	 --amazonec2-ami ${AWS_AMI} --amazonec2-instance-type "${AWS_INSTANCE_TYPE}" --amazonec2-region "${AWS_DEFAULT_REGION}" --amazonec2-zone "${AWS_ZONE}" \
 	 --amazonec2-vpc-id "${AWS_VPC_ID}" --amazonec2-subnet-id "${AWS_SUBNET_ID}" --amazonec2-security-group "${AWS_SECURITY_GROUP}" \
 	 --amazonec2-private-address-only=true --engine-insecure-registry "${DOCKER_REGISTRY}" \
-	 --engine-env DOCKER_REGISTRY=${DOCKER_REGISTRY} --engine-env YR_VERSION=${VERSION} ${MACHINE_NAME}
+	 --engine-env DOCKER_REGISTRY=${DOCKER_REGISTRY} --engine-env YR_VERSION=${YR_VERSION} ${MACHINE_NAME}
 	 
 else
 
 	docker-machine create --driver $DOCKER_MACHINE_DRIVER --engine-insecure-registry "${DOCKER_REGISTRY}" \
-	--engine-env DOCKER_REGISTRY=${DOCKER_REGISTRY} --engine-env YR_VERSION=${VERSION} ${MACHINE_NAME}
+	--engine-env DOCKER_REGISTRY=${DOCKER_REGISTRY} --engine-env YR_VERSION=${YR_VERSION} ${MACHINE_NAME}
 
 fi
 	
@@ -51,4 +52,7 @@ eval "$(docker-machine env ${MACHINE_NAME})"
 
 DM_IP=`docker-machine ip ${MACHINE_NAME}`
 
-docker-compose up
+# Workaround for docker-machine bug with --engine-env
+sed -i -- 's/\$YR_VERSION/'${YR_VERSION}'/g' docker-compose.yml
+
+docker-compose up -d
