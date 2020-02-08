cb4d923955d24d59f92b85767c4ff7e564c7765e
liya <liya@tikalk.com>
2020-02-08 22:16:46 +0200

    updates

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-DOCKER_REGISTRY=localhost:5000
-
-
-if [ -z YR_VERSION ]
+if [[ "$YR_VERSION" == "" ]]
 then
 	echo "YR_VERSION must be set!"
 	exit 1
 fi
 echo "YR_VERSION=$YR_VERSION"
 
-if [ -z WORKSPACE ]
+if [[ "$WORKSPACE" == "" ]]
 then
 	WORKSPACE=${PWD}
 fi
@@ -18,10 +15,8 @@ echo "WORKSPACE=$WORKSPACE"
 
 echo "Build ynginx image for version ${YR_VERSION}"
 cd ${WORKSPACE}/ynginx
-docker build -t ${DOCKER_REGISTRY}/yremote/ynginx:${YR_VERSION} .
-docker push ${DOCKER_REGISTRY}/yremote/ynginx:${YR_VERSION}
+docker build -t yremote/ynginx:${YR_VERSION} .
 echo
 echo "Build yremote image for version ${YR_VERSION}"
 cd ${WORKSPACE}/yremote
-docker build -t ${DOCKER_REGISTRY}/yremote/yremote:${YR_VERSION} .
-docker push ${DOCKER_REGISTRY}/yremote/yremote:${YR_VERSION}
+docker build -t yremote/yremote:${YR_VERSION} .
